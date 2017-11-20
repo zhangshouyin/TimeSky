cc8c56c0947a5275ff064033389fe3e6c54890fd
zhangshouyin <zhangshouyin_good@163.com>
2017-11-20 13:44:14 +0800

    配置podspec

---
+++ LKTset/LKTset/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LKTset
+//
+//  Created by zhangshouyin on 2017/11/20.
+//  Copyright © 2017年 zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LKTset/LKTset/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LKTset
+//
+//  Created by zhangshouyin on 2017/11/20.
+//  Copyright © 2017年 zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
